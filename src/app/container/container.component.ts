9c8054ee303015a86ce363e4240f0d9ffe04823c
German Donoso <germanedt@gmail.com>
2018-09-13 18:01:59 -0500

    lista productos

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -6,34 +6,27 @@ import { StoreService } from '../services/store.service';
   styleUrls: ['./container.component.css']
 })
 export class ContainerComponent implements OnInit {
+  titleList: string;
   productslist: Array<{
-    id: number,
+    quantity: number,
+    price: string,
+    available: boolean,
+    sublevel_id: number,
     name: string,
-    products: Array<{
-      quantity: number,
-      price: string,
-      available: boolean,
-      sublevel_id: number,
-      name: string,
-      id: string
-    }>
+    id: string
   }>;
   constructor(public serviceStore: StoreService) {
     this.productslist = [];
+    this.titleList = 'Todos los productos';
   }
   ngOnInit() {
     this.serviceStore.listAllProducts().then((data) => {
-      console.log(this.serviceStore.categories);
       this.serviceStore.products = data.json()['products'];
-      /*this.serviceStore.categories.forEach(category => {
-        category.sublevels.forEach( level => {
-          level.sublevels.forEach( subLevel => {
-            console.log(subLevel);
-          });
-        });
-        this.productslist.push({id: category.id, name: category.name, products: []});
-      });*/
+      this.productslist = this.serviceStore.products;
+      console.log(this.serviceStore.products);
     });
   }
-
+  listaProductosCategoria(category) {
+    return this.serviceStore.products.filter( product => product.sublevel_id === category);
+  }
 }
